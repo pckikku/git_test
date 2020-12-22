57cc19dbf97caa23446667c5567c27006b0d24e3
kikku <60809011+pckikku@users.noreply.github.com>
2020-12-22 23:02:23 +0530

    Delete index.js

--- index.js
+++
@@ -1,5 +0,0 @@
-setInterval(()=>{
-    let colors = ['#df14d2','#a334e3','#10f344']
-    let pos = Math.floor(Math.random()*colors.length)
-    document.body.style.backgroundColor = colors[pos]
-},2000)
